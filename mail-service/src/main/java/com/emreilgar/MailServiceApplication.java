9f804921ce3331dc353957f44221ada4fe947d71
Emre ILGAR <emreilgar90@gmail.com>
2023-10-12 18:27:48 +0300

    Spring Security entegre edildi. PostService eklendi.

---
+++ mail-service/src/main/java/com/emreilgar/MailServiceApplication.java
@@ -0,0 +1,15 @@
+package com.emreilgar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MailServiceApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(MailServiceApplication.class);
+
+    }
+}
